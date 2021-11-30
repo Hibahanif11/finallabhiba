1177242760fa35f43656da8ed6517d8ae5665593
Hibahanif11 <92621554+Hibahanif11@users.noreply.github.com>
2021-11-30 12:29:56 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static int hoursWorked;
+        private static int basePay;
+        private static int overtimeHours;
+        private static int overtimePay;
+        private static int unitPay;
+        private static int hourlyPayRate;
+        private static int grosspay;
+      
+
+        public static int BASE_HOURS { get; private set; }
+        public static int OVERTIME_RATE { get; private set; }
+
+        private static void calculatesalaray()
+        {
+            if(hoursWorked > BASE_HOURS)
+            {
+                basePay = hourlyPayRate * BASE_HOURS;
+
+                overtimeHours = hoursWorked - BASE_HOURS;
+
+                overtimePay = overtimeHours * hourlyPayRate * OVERTIME_RATE;
+
+                unitPay = 3* 5;
+
+                grosspay = basePay + overtimePay + unitPay;
+
+
+
+            }
+        }
+    }
+}
---
+++ monthly.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Monthly
+    {
+        public void calculateSalary()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
